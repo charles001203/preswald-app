530a0d101a7868d9b5767590ebf37aad9cd1c20d
Charles Yu <yuruntang1203@gmail.com>
2025-05-17 11:52:09 -0400

    minor changes

--- hello.py
+++ hello.py
@@ -1,12 +1,19 @@
 from preswald import connect, get_df
 
 connect()  # Initialize connection to preswald.toml data sources
-df = get_df("student_habits_performance.csv")  # Load data
+df = get_df('data/student_habits_performance.csv')  # Load data
 
 from preswald import query
 
-sql = "SELECT * FROM student_habits_performance.csv WHERE study_hours_per_day > 3"
-filtered_df = query(sql, "student_habits_performance.csv")
+# query students who study more than 3 hours per day (show good work ethics!)
+try:
+    sql = "SELECT * FROM student_habits_performance WHERE study_hours_per_day > 3"
+except ValueError as e:
+    print(f"Configuration error: {e}")
+except Exception as e:
+    print(f"Query error: {e}")
+
+filtered_df = query(sql, 'data/student_habits_performance.csv')
 
 from preswald import table, text
 
@@ -16,5 +23,6 @@ table(filtered_df, title="Filtered Data")
 from preswald import plotly
 import plotly.express as px
 
-fig = px.scatter(df, x="study_hours_per_day", y="exam_score", color="category")
+# identify the relationships between parent education level and good students' exam scores
+fig = px.bar(filtered_df, x="parental_education_level", y="exam_score")
 plotly(fig)
